68dcb7b006ce4af5019bc709344d870d507526e6
tinh cao <caodangtinh@gmail.com>
2018-07-03 12:05:19 +0700

    add display synonym

--- src/main/java/com/tinhcao/analysis/synonym/SynonymFilter.java
+++ src/main/java/com/tinhcao/analysis/synonym/SynonymFilter.java
@@ -25,63 +25,57 @@ import java.util.Stack;
 
 // From chapter 4
 public class SynonymFilter extends TokenFilter {
-  public static final String TOKEN_TYPE_SYNONYM = "SYNONYM";
+	public static final String TOKEN_TYPE_SYNONYM = "SYNONYM";
 
-  private Stack<String> synonymStack;
-  private SynonymEngine engine;
-  private AttributeSource.State current;
+	private Stack<String> synonymStack;
+	private SynonymEngine engine;
+	private AttributeSource.State current;
 
-  private final TermAttribute termAtt;
-  private final PositionIncrementAttribute posIncrAtt;
+	private final TermAttribute termAtt;
+	private final PositionIncrementAttribute posIncrAtt;
 
-  public SynonymFilter(TokenStream in, SynonymEngine engine) {
-    super(in);
-    synonymStack = new Stack<String>();                     //#1 
-    this.engine = engine;
+	public SynonymFilter(TokenStream in, SynonymEngine engine) {
+		super(in);
+		synonymStack = new Stack<String>(); // #1
+		this.engine = engine;
 
-    this.termAtt = addAttribute(TermAttribute.class);
-    this.posIncrAtt = addAttribute(PositionIncrementAttribute.class);
-  }
+		this.termAtt = addAttribute(TermAttribute.class);
+		this.posIncrAtt = addAttribute(PositionIncrementAttribute.class);
+	}
 
-  public boolean incrementToken() throws IOException {
-    if (synonymStack.size() > 0) {                          //#2
-      String syn = synonymStack.pop();                      //#2
-      restoreState(current);                                //#2
-      termAtt.setTermBuffer(syn);
-      posIncrAtt.setPositionIncrement(0);                   //#3
-      return true;
-    }
+	public boolean incrementToken() throws IOException {
+		if (synonymStack.size() > 0) { // #2
+			String syn = synonymStack.pop(); // #2
+			restoreState(current); // #2
+			termAtt.setTermBuffer(syn);
+			posIncrAtt.setPositionIncrement(0); // #3
+			return true;
+		}
 
-    if (!input.incrementToken())                            //#4  
-      return false;
+		if (!input.incrementToken()) // #4
+			return false;
 
-    if (addAliasesToStack()) {                              //#5 
-      current = captureState();                             //#6
-    }
+		if (addAliasesToStack()) { // #5
+			current = captureState(); // #6
+		}
 
-    return true;                                            //#7
-  }
+		return true; // #7
+	}
 
-  private boolean addAliasesToStack() throws IOException {
-    String[] synonyms = engine.getSynonyms(termAtt.term()); //#8
-    if (synonyms == null) {
-      return false;
-    }
-    for (String synonym : synonyms) {                       //#9
-      synonymStack.push(synonym);
-    }
-    return true;
-  }
+	private boolean addAliasesToStack() throws IOException {
+		String[] synonyms = engine.getSynonyms(termAtt.term()); // #8
+		if (synonyms == null) {
+			return false;
+		}
+		for (String synonym : synonyms) { // #9
+			synonymStack.push(synonym);
+		}
+		return true;
+	}
 }
 
 /*
-#1 Define synonym buffer
-#2 Pop buffered synonyms
-#3 Set position increment to 0
-#4 Read next token
-#5 Push synonyms onto stack
-#6 Save current token
-#7 Return current token
-#8 Retrieve synonyms
-#9 Push synonyms onto stack
-*/
+ * #1 Define synonym buffer #2 Pop buffered synonyms #3 Set position increment
+ * to 0 #4 Read next token #5 Push synonyms onto stack #6 Save current token #7
+ * Return current token #8 Retrieve synonyms #9 Push synonyms onto stack
+ */
--- src/main/java/com/tinhcao/analysis/synonym/WordNetSynonymEngine.java
+++ src/main/java/com/tinhcao/analysis/synonym/WordNetSynonymEngine.java
@@ -1,5 +1,10 @@
 package com.tinhcao.analysis.synonym;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Copyright Manning Publications Co.
  *
@@ -18,60 +23,51 @@ package com.tinhcao.analysis.synonym;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 import com.tinhcao.common.AllDocCollector;
 
-import org.apache.lucene.store.Directory;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Iterator;
-
-
 // From chapter 9
 public class WordNetSynonymEngine implements SynonymEngine {
-  IndexSearcher searcher;
-  Directory fsDir;
+	IndexSearcher searcher;
+	Directory fsDir;
 
-  public WordNetSynonymEngine(File index) throws IOException {
-    fsDir = FSDirectory.open(index);
-    searcher = new IndexSearcher(fsDir);
-  }
+	public WordNetSynonymEngine(File index) throws IOException {
+		fsDir = FSDirectory.open(index);
+		searcher = new IndexSearcher(fsDir);
+	}
 
-  public void close() throws IOException {
-    searcher.close();
-    fsDir.close();
-  }
+	public void close() throws IOException {
+		searcher.close();
+		fsDir.close();
+	}
 
-  public String[] getSynonyms(String word) throws IOException {
+	public String[] getSynonyms(String word) throws IOException {
 
-    List<String> synList = new ArrayList<String>();
+		List<String> synList = new ArrayList<String>();
 
-    AllDocCollector collector = new AllDocCollector();  // #A
+		AllDocCollector collector = new AllDocCollector(); // #A
 
-    searcher.search(new TermQuery(new Term("word", word)), collector);
+		searcher.search(new TermQuery(new Term("word", word)), collector);
 
-    for(ScoreDoc hit : collector.getHits()) {    // #B
-      Document doc = searcher.doc(hit.doc);
+		for (ScoreDoc hit : collector.getHits()) { // #B
+			Document doc = searcher.doc(hit.doc);
 
-      String[] values = doc.getValues("syn");
+			String[] values = doc.getValues("syn");
 
-      for (String syn : values) {  // #C
-        synList.add(syn);
-      }
-    }
+			for (String syn : values) { // #C
+				synList.add(syn);
+			}
+		}
 
-    return synList.toArray(new String[0]);
-  }
+		return synList.toArray(new String[0]);
+	}
 }
 
 /*
-  #A Collect every matching document
-  #B Iterate over matching documents
-  #C Record synonyms
-*/
+ * #A Collect every matching document #B Iterate over matching documents #C
+ * Record synonyms
+ */
